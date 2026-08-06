3454911ac0ba2d9af24c854726dccc120cea79fc
iremcan <irem.1206.can@gmail.com>
2026-08-06 13:05:46 +0300

    Refactor prediction handling in app.py

--- app.py
+++ app.py
@@ -86,31 +86,26 @@ def roboflow_tahmin_yap(frame):
             return frame_rgb, "Bilinmeyen", 0.0
 
         processed_frame = frame_rgb.copy()
-        best_class = "Bilinmeyen"
-        max_conf = 0.0
+        
+        best_pred = max(predictions, key=lambda x: float(x['confidence']))
+        
+        x, y, w, h = int(best_pred['x']), int(best_pred['y']), int(best_pred['width']), int(best_pred['height'])
+        best_class = str(best_pred['class']).upper()
+        max_conf = float(best_pred['confidence'])
 
-        for pred in predictions:
-            x, y, w, h = int(pred['x']), int(pred['y']), int(pred['width']), int(pred['height'])
-            label = str(pred['class']).upper()
-            conf = float(pred['confidence'])
+        pt1 = (int(x - w / 2), int(y - h / 2))
+        pt2 = (int(x + w / 2), int(y + h / 2))
 
-            if conf > max_conf:
-                max_conf = conf
-                best_class = label
-
-            pt1 = (int(x - w / 2), int(y - h / 2))
-            pt2 = (int(x + w / 2), int(y + h / 2))
-
-            cv2.rectangle(processed_frame, pt1, pt2, (0, 255, 0), 3)
-            cv2.putText(
-                processed_frame, 
-                f"{label} %{int(conf*100)}", 
-                (pt1[0], max(pt1[1] - 10, 20)),
-                cv2.FONT_HERSHEY_SIMPLEX, 
-                0.9, 
-                (0, 255, 0), 
-                2
-            )
+        cv2.rectangle(processed_frame, pt1, pt2, (0, 255, 0), 3)
+        cv2.putText(
+            processed_frame, 
+            f"{best_class} %{int(max_conf*100)}", 
+            (pt1[0], max(pt1[1] - 10, 20)),
+            cv2.FONT_HERSHEY_SIMPLEX, 
+            0.9, 
+            (0, 255, 0), 
+            2
+        )
 
         return processed_frame, best_class, max_conf
 
